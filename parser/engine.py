19535cc7fea8e16dfe887f69e3bb5d21eaa726a1
Alexander Pavlov <dev@pavlovteam.ru>
2024-11-28 13:43:55 +0300

    build: refactor config

--- parser/engine.py
+++ parser/engine.py
@@ -1,12 +1,15 @@
 import asyncio
+import os
+
 from parser.bot.messages import start_bot
 from parser.database.database import update_price
 
 
 async def run_every_minute(func):
+    time_update = int(os.environ.get('TIME'))
     while True:
         await func()
-        await asyncio.sleep(60)
+        await asyncio.sleep(time_update)
 
 
 async def main():
