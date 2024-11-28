62821a776311ae907ed4ec611b5f9fe5772d527b
Alexander Pavlov <dev@pavlovteam.ru>
2024-11-28 10:37:17 +0300

    build: refactor config

--- parser/database/config.py
+++ parser/database/config.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://root:password@mongodb:27017/')
+client = MongoClient('mongodb://mongodb:27017/')
 
 telegram_user_db = client['telegram_user']
 
